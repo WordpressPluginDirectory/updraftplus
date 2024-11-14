0f499e5242bdd180123c5e72b4572578e32f6d44
Justin Gardner <justin@rhynorater.com>
2024-11-14 06:01:49 -0500

    1.24.8

--- updraftplus/central/updraftplus.php
+++ updraftplus/central/updraftplus.php
@@ -39,6 +39,7 @@ class UpdraftPlus_Host extends UpdraftCentral_Host {
 		parent::__construct();
 		
 		add_action('updraftplus_debugtools_dashboard', array($this, 'debugtools_dashboard'), 20);
+		add_action('updraftplus_load_translations_for_udcentral', array($this, 'load_updraftplus_translations'));
 
 		$this->maybe_initialize_required_objects();
 	}
@@ -268,9 +269,16 @@ class UpdraftPlus_Host extends UpdraftCentral_Host {
 			}
 		}
 
+		$this->load_updraftplus_translations();
+	}
+
+	/**
+	 * Load translations which are based on UpdraftPlus domain text
+	 */
+	public function load_updraftplus_translations() {
 		// Load updraftplus translations
 		if (defined('UPDRAFTCENTRAL_CLIENT_DIR') && file_exists(UPDRAFTCENTRAL_CLIENT_DIR.'/translations-central.php')) {
-			$this->translations = include_once(UPDRAFTCENTRAL_CLIENT_DIR.'/translations-central.php');
+			$this->translations = include(UPDRAFTCENTRAL_CLIENT_DIR.'/translations-central.php');
 		}
 	}
 }
--- updraftplus/vendor/composer/installed.php
+++ updraftplus/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'updraftplus/updraftplus',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '78e36bf920c6a9dfbc891990388b12b2694148ba',
+        'reference' => '2b445b59428212ae025f0f55deedee5504ea37af',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -355,7 +355,7 @@
         'updraftplus/updraftplus' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '78e36bf920c6a9dfbc891990388b12b2694148ba',
+            'reference' => '2b445b59428212ae025f0f55deedee5504ea37af',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
